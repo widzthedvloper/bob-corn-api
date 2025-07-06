15850a23904a8b5826b781c0c09bb8de76a8f97f
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-05 20:51:20 -0400

    Create controller to get a buyer

--- controllers/buyer-controllers.js
+++ controllers/buyer-controllers.js
@@ -5,6 +5,19 @@ async function getBuyers(req, res) {
     res.json(buyers)
 }
 
+async function getBuyer(req, res){
+    try {
+        const buyerId = parseInt(req?.params?.buyerId)
+        if(buyerId){
+            const buyer = await buyersTable.findByPk(buyerId)
+            res.json(buyer)
+        }
+    } catch (error) {
+        res.status(404).json({error: 'Buyer not found'})
+    }
+}
+
 module.exports = {
-    getBuyers
+    getBuyers,
+    getBuyer
 }
--- routes/buyers.routes.js
+++ routes/buyers.routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 buyersRouter = express.Router()
 
-const {getBuyers} = require('../controllers/buyer-controllers')
+const {getBuyers, getBuyer} = require('../controllers/buyer-controllers')
 
 
 buyersRouter.get('/', getBuyers)
 buyersRouter.post('/', (req, res)=>{})
-buyersRouter.get('/:buyerId', (req, res)=>{})
+buyersRouter.get('/:buyerId', getBuyer)
 buyersRouter.post('/:buyerId', (req, res)=>{})
 
 module.exports = {buyersRouter};
