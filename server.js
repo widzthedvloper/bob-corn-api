d4bef4a3bac68a0c8053e787a5879c67eda44918
Widzmarc Phelle <widzmarc@gmail.com>
2025-07-05 20:18:16 -0400

    Install Sequilize to work with postgres

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const {buyersRouter} = require('./routes/buyers.routes')
 
 const app = express();
 
@@ -6,9 +8,15 @@ const PORT = 8080;
 
 app.use(express.json())
 
+app.use(cors({
+    origin: 'http://localhost:8080',
+}))
+
 app.get('/', (req, res)=>{
     res.send('hello!')
 })
+
+app.use('/buyers', buyersRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
